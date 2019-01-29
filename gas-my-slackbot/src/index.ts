8e91649fdcab19bc7a469f038449919ff3a0561e
tic40 <ccpzjoh@gmail.com>
2019-01-30 00:47:03 +0900

    Update

--- gas-my-slackbot/src/index.ts
+++ gas-my-slackbot/src/index.ts
@@ -1,21 +1,3 @@
-const properties = PropertiesService.getScriptProperties()
-// slack
-const SLACK_WEBHOOK_URL: string = properties.getProperty('SLACK_WEBHOOK_URL')
-const SLACK_CHANNEL: string = properties.getProperty('SLACK_CHANNEL')
-const SLACK_BOT_ICON_EMOJI: string =
-  properties.getProperty('SLACK_BOT_ICON_EMOJI') || ':sunglasses:'
-const SLACK_BOT_USERNAME: string =
-  properties.getProperty('SLACK_BOT_USERNAME') || 'gas-my-slack-channel-bot'
-const SLACK_BOT_ATTACHMENT_COLOR: string =
-  properties.getProperty('SLACK_BOT_ATTACHMENT_COLOR') || '#ffa500'
-// GitHuib
-const GITHUB_USER_ID: string = properties.getProperty('GITHUB_USER_ID')
-
-// crowi
-const CROWI_USER_ID: string = properties.getProperty('CROWI_USER_ID')
-const CROWI_API_TOKEN: string = properties.getProperty('CROWI_API_TOKEN')
-const CROWI_URL: string = properties.getProperty('CROWI_URL')
-
 const isWeekend = (): boolean => {
   const day: number = new Date().getDay()
   return day === 0 || day === 6
@@ -199,5 +181,5 @@ This page has been created by a script. Please write your diary today and delete
 }
 
 function sendTestMessageToSlack(): void {
-  postToSlack('Hello! This is a test message from gas-my-slack-channel-bot')
+  postToSlack('Hello! This is a test message')
 }
