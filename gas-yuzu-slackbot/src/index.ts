10743b79436f03f953b4cead52cd67b52d83054c
tic40 <ccpzjoh@gmail.com>
2023-06-14 09:32:43 +0900

    Update

--- gas-yuzu-slackbot/src/index.ts
+++ gas-yuzu-slackbot/src/index.ts
@@ -12,12 +12,6 @@ const doPost = (e): void => {
     return
   }
 
-  if (!message) {
-    const response: any[] = getSpreadSheetValues(SHEET_NAMES.RESPONSE)
-    postToSlack(randomFromArray(response)[0], channelName)
-    return
-  }
-
   if (new RegExp('(.*)(post:)(.+)').test(message)) {
     const matched: string[] = message.match(new RegExp('(.*)(post:)(.+)'))
     const text: string = matched[matched.length - 1]
@@ -37,102 +31,17 @@ const doPost = (e): void => {
     return
   }
 
-  /*
-  if (
-    new RegExp('ほしいもの|欲しいもの|欲しい物|wishlist', 'i').test(message)
-  ) {
-    const lows: any[] = getSpreadSheetValues(SHEET_NAMES.WISHLIST)
-    const messages = [`みんなのほしいもの${BOT_PHRASE}`]
-    const wishlistUrl: string = getSpreadSheetUrl(SHEET_NAMES.WISHLIST)
-    for (const low of lows) {
-      messages.push(`${low[0]}: ${low[1]}`)
-    }
-    if (!messages) {
-      return
-    }
-    postToSlack(messages.join('\n'), channelName)
-    return
-  }
-  */
-
-  if (new RegExp('(って|とは)(なに|何)').test(message)) {
-    const replacedMessage: string = message.replace(/,|、/g, '')
-    const q: string = replacedMessage.match(
-      new RegExp('(.*?)(って|とは)(なに|何)')
-    )[1]
-    const urlAndBody: any = getWikipediaUrlAndBody(q.trim())
-    if (!urlAndBody) {
-      postToSlack(`...それは知らない${BOT_PHRASE}`, channelName)
-      return
-    }
-    postToSlack(`${urlAndBody.body.substr(0, 140)}...`, channelName)
-    postToSlack(`詳しくはここ見てな\n${urlAndBody.url}`, channelName)
-    return
-  }
-
-  if (new RegExp(`(.*)(翻訳して|翻訳)(.+)`).test(message)) {
-    const matched: string[] = message.match(
-      new RegExp(`(.*)(翻訳して|翻訳)(.+)`)
-    )
-    const text: string = matched[matched.length - 1]
-    const translatedText: string = googleTranslate(text.trim()) || ''
-    postToSlack(`${translatedText} ${BOT_PHRASE}`, channelName)
-    return
-  }
-
-  if (new RegExp('天気').test(message)) {
-    let dayId: number = WEATHER_FORECAST_DAY_ID.TODAY
-    if (message.indexOf('明日') !== -1) {
-      dayId = WEATHER_FORECAST_DAY_ID.TOMORROW
-    } else if (message.indexOf('明後日') !== -1) {
-      dayId = WEATHER_FORECAST_DAY_ID.DAY_AFTER_TOMORROW
-    }
-    postToSlack(weatherForecast(dayId), channelName)
-    return
-  }
-
   postToSlack(getUserLocalMessage(message), channelName)
   return
-}
-
-function openingCall(): void {
-  const messages: any[] = getSpreadSheetValues(SHEET_NAMES.MORNING_CALL)
-  postToSlack([randomFromArray(messages)[0], whatTheDay()].join('\n'))
-}
-
-/*
-function dailyTodo(): void {
-  const message: string = `今日やるべきことをやろう${BOT_PHRASE}！`
-  const trello: string = 'https://trello.com/b/Dk9SG77Y/kazukoやることリスト'
-  postToSlack([message, trello].join('\n'))
-}
-*/
-
-function eveningCall(): void {
-  const messages: any[] = getSpreadSheetValues(SHEET_NAMES.EVENING_CALL)
-  postToSlack(
-    [
-      randomFromArray(messages)[0],
-      weatherForecast(WEATHER_FORECAST_DAY_ID.TOMORROW)
-    ].join('\n')
-  )
 }
 
 function trendReport(): void {
   postToSlack(
     [
-      `現在のトレンドだよ。もうチェックした${BOT_PHRASE}？`,
+      `現在のトレンドだよ。もうチェックした？`,
       `${getTwitterTrendsMessage(getTwitterTrends(23424856))}`
     ].join('\n')
   )
-}
-
-function tweet(): void {
-  const randInt = Math.floor(Math.random() * Math.floor(5))
-  if (randInt === 1) {
-    const messages: any[] = getSpreadSheetValues(SHEET_NAMES.TWEET)
-    postToSlack(randomFromArray(messages)[0])
-  }
 }
 
 function test(): void {
--- gas-yuzu-slackbot/src/services.ts
+++ gas-yuzu-slackbot/src/services.ts
@@ -24,56 +24,6 @@ const postToSlack = (
   })
 }
 
-const getWeatherForecast = (cityId: number) => {
-  // ref: http://weather.livedoor.com/weather_hacks/webservice
-  const baseUrl: string =
-    'http://weather.livedoor.com/forecast/webservice/json/v1'
-  const res: any = UrlFetchApp.fetch(`${baseUrl}?city=${cityId}`, {
-    method: 'get'
-  })
-  if (!res) {
-    return {}
-  }
-  return JSON.parse(res.getContentText())
-}
-
-const formatWeatherForecastMessage = ({
-  dayId,
-  forecastData
-}: {
-  dayId: number
-  forecastData: any
-}): string => {
-  const telop: string = forecastData.forecasts[dayId].telop
-  const areaName: string = forecastData.location.city
-  const temp = forecastData.forecasts[dayId].temperature
-  const min: number = Number(temp.min && temp.min.celsius)
-  const max: number = Number(temp.max && temp.max.celsius)
-  let comment: string = ''
-  if (/雨/.test(telop)) {
-    comment = `傘が必要かもしれん${BOT_PHRASE}`
-  }
-
-  const unknownText: string = '-'
-  return [
-    `${areaName}: ${telop}`,
-    `気温(最低/最高): ${min || unknownText} / ${max || unknownText}`,
-    comment
-  ].join('\n')
-}
-
-const weatherForecast = (dayId: number): string => {
-  const weatherTokyo = getWeatherForecast(130010)
-  if (!weatherTokyo) {
-    return
-  }
-  const targetDays: string[] = ['今日', '明日', '明後日']
-  return [
-    `${targetDays[dayId]}の天気${BOT_PHRASE}!`,
-    formatWeatherForecastMessage({ dayId, forecastData: weatherTokyo })
-  ].join('\n')
-}
-
 const getUserLocalMessage = (text: string): string => {
   const url: string = `https://chatbot-api.userlocal.jp/api/chat?key=${USER_LOCAL_API_KEY}&message=${text}&bot_name=${SLACK_BOT_USERNAME}`
   const res = UrlFetchApp.fetch(url)
@@ -98,29 +48,6 @@ const getTwitterTrendsMessage = (trends: any[], limit = 10): string => {
     .join('\n')
 }
 
-const whatTheDay = (): string => {
-  const lows: any[] = getSpreadSheetValues(SHEET_NAMES.ANNIVERSARY)
-  const d: Date = new Date()
-  const today: string = `${d.getMonth() + 1}/${d.getDate()}`
-  for (const low of lows) {
-    if (low[0] === today) {
-      const subject: string = low[1]
-      const target: string = low[2]
-      if (!subject) {
-        return
-      }
-
-      if (subject === '誕生日') {
-        postToLine(`今日は${target}の誕生日！おめでと${BOT_PHRASE}！`)
-      } else {
-        postToLine(`今日は${subject}だ${BOT_PHRASE}！`)
-      }
-      return
-    }
-  }
-  return `今日は${today}、今年も残り \`${daysLeft()}日\` ${BOT_PHRASE}`
-}
-
 const getWikipediaUrlAndBody = (q: string): { url: string; body: string } => {
   const simpleWikipediaApi: string = 'http://wikipedia.simpleapi.net/api'
   const url: string = `${simpleWikipediaApi}?keyword=${encodeURIComponent(
@@ -133,22 +60,5 @@ const getWikipediaUrlAndBody = (q: string): { url: string; body: string } => {
   return {
     url: res[0].url,
     body: res[0].body
-  }
-}
-
-const googleTranslate = (text: string, from = 'ja', to = 'en'): any => {
-  return LanguageApp.translate(text, from, to)
-}
-
-const getLastWithingsMeasure = (): any => {
-  const sheet: any = getSpreadSheet(SHEET_NAMES.WITHINGS)
-  const lastRow: number = sheet.getLastRow()
-  return {
-    username: sheet.getRange(lastRow, WITHINGS_COLUMNS.USERNAME).getValue(),
-    weight: sheet.getRange(lastRow, WITHINGS_COLUMNS.WEIGHT).getValue(),
-    fatPercent: sheet
-      .getRange(lastRow, WITHINGS_COLUMNS.FAT_PERCENT)
-      .getValue(),
-    date: sheet.getRange(lastRow, WITHINGS_COLUMNS.DATE).getValue()
   }
 }
